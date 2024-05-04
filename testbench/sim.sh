96fb3c60f87247af2d551c29a7609dfcd374948d
sirofen <68060514+sirofen@users.noreply.github.com>
2024-05-04 19:59:36 +0300

    rm comments

--- testbench/sim.sh
+++ testbench/sim.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-#cd ../
-#iverilog -o testbench/out.sv -E $1
-#cd testbench
 docker run -ti -v ${PWD}:/work verilator/verilator:latest --timing -Wno-WIDTH -Wno-MULTIDRIVEN -Wno-LATCH --cc --exe --trace --trace-fst --trace-structs --trace-params --build out.sv sim.cpp 
-#./obj_dir/Vout
